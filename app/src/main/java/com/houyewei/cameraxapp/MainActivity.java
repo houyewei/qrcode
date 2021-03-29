4f340afdea0de3b9076ef8e06e83e950aa730dad
Wynston <houyewei@gmail.com>
2021-03-29 21:37:43 +0800

    change class & layout names

--- app/src/main/java/com/houyewei/cameraxapp/MainActivity.java
+++ app/src/main/java/com/houyewei/cameraxapp/MainActivity.java
@@ -21,15 +21,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         viewModel = new ViewModelProvider(this).get(BarcodeViewModel.class);
         viewModel.getBarcode().observe(this, item -> {
             // Perform an action with the latest item data
